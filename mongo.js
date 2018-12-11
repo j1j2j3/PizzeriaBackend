1cb679d288c0cb1a8e0877df8aa2404324d2130e
Jarkko <jarkko.niemi95@hotmail.com>
2018-12-11 02:40:43 +0200

    toinen yritys

--- mongo.js
+++ mongo.js
@@ -4,17 +4,30 @@ const mongoose = require('mongoose')
 const url = 'mongodb://pizza:pizza123@ds163034.mlab.com:63034/pizza'
 
 mongoose.connect(url)
+mongoose.Promise = global.Promise
 
-//skeema
-const pizzaSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean
+const Pizza = mongoose.model('Pizza', {
+  content: String,
+  date: Date
+})
+
+Pizza
+  .find({})
+  .then(result => {
+    result.forEach(pizza => {
+      console.log(pizza)
+    })
+    mongoose.connection.close()
   })
-  
-  const Pizza = mongoose.model('Pizza', pizzaSchema);
+//skeema
+/*const pizzaSchema = new mongoose.Schema({
+    content: String,
+    date: Date
+  })
+  */
+  //const Pizza = mongoose.model('Pizza', pizzaSchema);
 
-
+/*
 const pizza = new Pizza({
     content: 'Testipizza',
     date: new Date(),
@@ -30,3 +43,4 @@ const pizza = new Pizza({
     })
     mongoose.connection.close()
   })
+  */
